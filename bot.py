8eece0a05a65b2f72505a842f282e83e26ff9f7c
vish25kum-coder <vish25kum@gmail.com>
2026-02-14 18:34:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,26 @@
-import os
 import telebot
 import google.generativeai as genai
 
-# ENV variables
-TELEGRAM_TOKEN = os.getenv("AIzaSyCusRWxXMju-lZx0tw2nfUXuYjq5Xk3Dw4")
-GEMINI_API_KEY = os.getenv("8031656712:AAGJBxJqliV7KskwUUZQcYDU2gf1Fv8g6W8")
+# TOKENS
+TELEGRAM_TOKEN = "8031656712:AAGJBxJqliV7KskwUUZQcYDU2gf1Fv8g6W8"
+GEMINI_API_KEY = "AIzaSyCusRWxXMju-lZx0tw2nfUXuYjq5Xk3Dw4"
 
 # Configure Gemini
 genai.configure(api_key=GEMINI_API_KEY)
-
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Create bot
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-
-# Start command
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Hello! Main Mr Bear AI hoon. Mujhse kuch bhi pucho.")
-
-
-# Handle all messages
 @bot.message_handler(func=lambda message: True)
 def reply(message):
     try:
-        response = model.generate_content(message.text)
+        user_text = message.text
+        response = model.generate_content(user_text)
         bot.reply_to(message, response.text)
     except Exception as e:
         bot.reply_to(message, "Error: " + str(e))
 
+print("Bot started...")
 
-# Run bot
-print("Bot is running...")
-bot.infinity_polling(skip_pending=True, none_stop=True)
+bot.infinity_polling()
